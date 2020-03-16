a54cc85146c12b4cd58050274d6662d14c3617bd
Sonoko Mizuki <sonoko@mizuki.io>
2020-03-17 00:29:12 +0900

    remove default components

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,16 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        apizu-site
-      </h1>
-      <h2 class="subtitle">
-        My breathtaking Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <img src="/img/top.png" class="top-image" />
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  }
-}
+  components: {}
+};
 </script>
 
-<style>
+<style lang="postcss" scoped>
 .container {
   margin: 0 auto;
   min-height: 100vh;
@@ -46,27 +18,10 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  .top-image {
+    width: 30%;
+    pointer-events: none;
+    user-select: none;
+  }
 }
 </style>
