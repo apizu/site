6c5f548b5a82ab0956de6a9d7e9789f763b68960
Sonoko Mizuki <sonoko@mizuki.io>
2020-03-17 00:58:49 +0900

    add sample code

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container">
     <img src="/img/top.png" class="top-image" />
-    <div>Aizu for Computer.</div>
+    <div>
+      <div class="message">Aizu for Computer.</div>
+      <div class="code">
+        <pre v-highlightjs><code class="sh">$ curl {WIP}</code></pre>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,6 +29,13 @@ export default {
     width: 30%;
     pointer-events: none;
     user-select: none;
+  }
+
+  .message {
+    padding: 8px 0;
+  }
+  .code {
+    padding: 8px 0;
   }
 }
 </style>
