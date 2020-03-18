6f710f7f435b4a16d3405fc2e1724456e2bb7401
Sonoko Mizuki <sonoko@mizuki.io>
2020-03-19 00:04:04 +0900

    add infformation components

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,51 @@
 <template>
   <div class="container">
-    <img src="/img/top.jpg" class="top-image" />
-    <div>
-      <div class="message">Aizu for Computer.</div>
-      <div class="code">
-        <pre v-highlightjs><code class="sh">$ curl {WIP}</code></pre>
+    <div class="top">
+      <img src="/img/top.jpg" class="top-image" />
+      <div>
+        <div class="message">Aizu for Computer.</div>
+        <div class="code">
+          <pre v-highlightjs><code class="sh">$ curl {WIP}</code></pre>
+        </div>
       </div>
     </div>
+    <information />
   </div>
 </template>
 
 <script>
+import Information from "@/components/information.vue";
+
 export default {
-  components: {}
+  components: {
+    Information
+  }
 };
 </script>
 
 <style lang="postcss" scoped>
 .container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+  width: 100%;
   color: #e4decd;
-  .top-image {
-    width: 30%;
-    pointer-events: none;
-    user-select: none;
-  }
+  .top {
+    margin: 0 auto;
+    min-height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    .top-image {
+      width: 30%;
+      pointer-events: none;
+      user-select: none;
+    }
 
-  .message {
-    padding: 8px 0;
-  }
-  .code {
-    padding: 8px 0;
+    .message {
+      padding: 8px 0;
+    }
+    .code {
+      padding: 8px 0;
+    }
   }
 }
 </style>
